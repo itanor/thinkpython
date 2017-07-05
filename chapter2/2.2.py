652f0e42da5bf136dcd75f42ced5a311662620a4
Itanor Strapazzon <itanor@gmail.com>
2017-07-05 08:54:31 -0300

    first exercises from chapter

---
+++ chapter2/2.2.py
@@ -0,0 +1,18 @@
+def show_value_and_type(value, type_of):
+  print('type of ' + value + ' is: ' + type_of)
+
+width = 17
+height = 12.0
+delimiter = '.'
+
+a1 = width / 2
+#print('type of ' + str(a1) + ' is: ' + str(type(a1)))
+show_value_and_type(str(a1), str(type(a1)))
+
+a2 = width / 2.0
+#print()
+
+print(height / 3)
+print(1 + 2 * 5)
+print(delimiter * 5)
+
