66ef88ccd3a06f1d4724bccd096a38d1767b351e
Itanor Strapazzon <itanor@gmail.com>
2017-07-07 08:35:48 -0300

    last part of exercise 2.2

--- chapter2/2.2.py
+++ chapter2/2.2.py
@@ -5,14 +5,19 @@ width = 17
 height = 12.0
 delimiter = '.'
 
-a1 = width / 2
-#print('type of ' + str(a1) + ' is: ' + str(type(a1)))
-show_value_and_type(str(a1), str(type(a1)))
+w1 = width / 2
+show_value_and_type(str(w1), str(type(w1)))
 
-a2 = width / 2.0
-#print()
+w2 = width / 2.0
+show_value_and_type(str(w2), str(type(w2)))
 
-print(height / 3)
-print(1 + 2 * 5)
-print(delimiter * 5)
+h1 = height / 3
+show_value_and_type(str(h1), str(type(h1)))
+
+# PEMDAS: Parentheses, Exponentiation, Multiplication and Division, Addition and Subtraction
+precedence = 1 + 2 * 5
+show_value_and_type(str(precedence), str(type(precedence)))
+
+points = delimiter * 5
+show_value_and_type(points, str(type(points)))
 
